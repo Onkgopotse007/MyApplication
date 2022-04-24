be4611ff346a2485aae113183be09073ae8fbc75
Onkgopotse007 <rt19001209@studentmail.biust.ac.bw>
2022-04-24 16:51:20 +0200

    Sunday 24 All pickers wo

--- app/src/main/java/com/example/myapplication/TimePicker.java
+++ app/src/main/java/com/example/myapplication/TimePicker.java
@@ -1,4 +1,26 @@
 package com.example.myapplication;
 
-public class TimePicker {
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class TimePicker extends androidx.fragment.app.DialogFragment {
+    @NonNull
+
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState){
+            Calendar c = Calendar.getInstance();
+            int mHour = c.get(Calendar.HOUR_OF_DAY);
+            int mMinute = c.get(Calendar.MINUTE);
+            return new TimePickerDialog(getActivity(), (TimePickerDialog.OnTimeSetListener)
+                    getActivity(),mHour,mMinute, true);
+
+            }
 }
