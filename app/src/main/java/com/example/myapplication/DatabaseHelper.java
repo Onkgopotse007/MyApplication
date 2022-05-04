daa1563156180c1247cb7e32a59f01b0cd235cc1
Onkgopotse007 <rt19001209@studentmail.biust.ac.bw>
2022-05-04 20:19:24 +0200

    Date and time picker app working Successfully

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -29,5 +31,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
         onCreate(sqLiteDatabase);
 
+    }
+    public Boolean insertDateTime( String selectedDate, String selectedTime){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL_1, selectedDate);
+        contentValues.put(COL_2, selectedTime);
+        long result = sqLiteDatabase.insert(TABLE_NAME,null,contentValues);
+        if(result == -1){
+            return false;
+        }else {
+            return true;
+        }
+
+    }
+    public Cursor getData(){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        Cursor cursor = sqLiteDatabase.rawQuery("Select * from "+TABLE_NAME,null);
+        return cursor;
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DateFormat;
 import java.util.Calendar;
@@ -17,7 +20,7 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener {
     DatabaseHelper myDB;
     TextView editDate, editTime;
-    Button dateButton, timeButton;
+    Button dateButton, timeButton, saveButton, viewButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,48 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         dateButton = findViewById(R.id.dateButton);
         editTime = findViewById(R.id.editTime);
         timeButton = findViewById(R.id.timeButton);
+        saveButton = findViewById(R.id.saveButton);
+        viewButton = findViewById(R.id.viewButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!editDate.getText().toString().isEmpty() && !editTime.getText().toString().isEmpty()) {
+                    String dateTXT = editDate.getText().toString();
+                    String timeTXT = editTime.getText().toString();
+                    Boolean insertDateTime = myDB.insertDateTime(dateTXT, timeTXT);
+                    if (insertDateTime == true) {
+                        Toast.makeText(MainActivity.this, "New Date time", Toast.LENGTH_SHORT).show();
+                        editDate.setText("");
+                        editTime.setText("");
+                    } else {
+                        Toast.makeText(MainActivity.this, "New Date time", Toast.LENGTH_SHORT).show();
+
+                    }
+                }else{
+                    Toast.makeText(MainActivity.this, "Date and time fields are empty", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        viewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor res = myDB.getData();
+                if(res.getCount() ==0){
+                    Toast.makeText(MainActivity.this, "No Date time exists",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while (res.moveToNext()){
+                    buffer.append("Date: "+res.getString(0)+"\n");
+                    buffer.append("Time: "+res.getString(1)+"\n");
+                }
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setCancelable(true);
+                builder.setTitle("Saved date and time");
+                builder.setMessage(buffer.toString());
+                builder.show();
+            }
+        });
         dateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
